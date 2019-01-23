6420347cf3aa0f698ea2fa549811bef067708bc0
jack <531739331@qq.com>
2019-01-23 20:57:11 +0800

    创建一个model

--- blog/models.py
+++ blog/models.py
@@ -2,4 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=32,default='Title')
+    content=models.TextField(null = True)
+
+#第一步python manage.py makemigrations
+#第二部python manage.py migrate
+#第三部python manage.py sqlmigrate blog 0001
